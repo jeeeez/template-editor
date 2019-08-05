3f017259193f5a09313bd7eb95baf15289c3b52f
JF <450994392@qq.com>
2019-08-05 23:42:56 +0800

    feat: createSpanReplacementNode support multiple classnames

--- packages/editor/src/createSpanReplacementNode.ts
+++ packages/editor/src/createSpanReplacementNode.ts
@@ -8,7 +8,11 @@ export function createSpanReplacementNode(innerHTML: string, className: string) 
   const $span = document.createElement('span');
   $span.innerHTML = innerHTML;
   if (className) {
-    $span.classList.add(className);
+    className.split(' ').forEach(subClassName => {
+      if (subClassName) {
+        $span.classList.add(subClassName);
+      }
+    });
   }
   return $span;
 }
